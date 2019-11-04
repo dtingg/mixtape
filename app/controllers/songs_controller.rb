e296c0217ff6c711f4af5813a7337c7e442e0841
Dianna Tingg <dtingg@gmail.com>
2019-11-04 05:40:34 -0800

    changed index to 12 random songs

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,7 @@
+class SongsController < ApplicationController
+  def index
+    # songs = Song.all
+    songs = Song.all.sample(12)
+    render :json => songs.as_json(only: [:id, :title, :artist, :year]), status: :ok
+  end
+end
